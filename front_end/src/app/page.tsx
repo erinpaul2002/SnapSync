b480f9ea5bd4b68136e3ef0d0607b85692b6dafd
GussySussy <gourisankar.shaji@gmail.com>
2024-04-26 12:17:30 +0530

    added supabase

--- front_end/src/app/page.tsx
+++ front_end/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { AuroraBackground } from "./components/ui/aurora_background";
 import './globals.css';
+import Notes from "./notes/page";
 
 export default function Home() {
   return (
-    <body className="bg-black">
-    <AuroraBackground className="bg-black"/>
+    <body className="bg-white">
+    <Notes />
     </body>
   );
 }

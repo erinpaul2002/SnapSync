a0e841c037bb4ca04245f8aaeb0a9f677ca750c6
GussySussy <gourisankar.shaji@gmail.com>
2024-04-26 20:52:19 +0530

    made gallery image containers uniform

--- front_end/src/app/components/ui/GalleryImageHolder.tsx
+++ front_end/src/app/components/ui/GalleryImageHolder.tsx
@@ -1,38 +1,35 @@
 import React from "react";
 import Image from "next/image";
 import { supabase } from "@/app/utils/supabase/supabase";
-import {saveAs} from "file-saver";
+import { saveAs } from "file-saver";
 interface GalleryImageHolderProps {
   imageUrl: string;
   title: string;
   subtitle: string;
 }
-const handleDownload = async (imageUrl:string,title:string) => {
-  saveAs(imageUrl,title)
-  
-}
-const deleteImage = async (imgpath:string) => {
+const handleDownload = async (imageUrl: string, title: string) => {
+  saveAs(imageUrl, title);
+};
+const deleteImage = async (imgpath: string) => {
   try {
-    const { data, error } = await supabase
-      .storage
-      .from('SnapSync Photos')
+    const { data, error } = await supabase.storage
+      .from("SnapSync Photos")
       .remove([imgpath]);
 
     if (error) {
       throw error;
     }
 
-    console.log('Image deleted successfully:', data);
-    
-  } catch (error:any) {
-    console.error('Error deleting image:', error.message);
+    console.log("Image deleted successfully:", data);
+  } catch (error: any) {
+    console.error("Error deleting image:", error.message);
   }
 };
-const handleDelete = async (imgurl:string) => {
+const handleDelete = async (imgurl: string) => {
   const url = new URL(imgurl);
-  const imagePathToDelete = url.pathname.split("/").slice(-2).join("/"); 
-  console.log(url)
-  console.log(imagePathToDelete)
+  const imagePathToDelete = url.pathname.split("/").slice(-2).join("/");
+  console.log(url);
+  console.log(imagePathToDelete);
   await deleteImage(imagePathToDelete);
 };
 
@@ -43,14 +40,18 @@ const GalleryImageHolder = ({
 }: GalleryImageHolderProps) => {
   return (
     <div className="lg:w-1/3 sm:w-1/2 p-4">
-      <div className="flex relative">
+      <div
+        className="flex relative"
+        style={{ width: "400px", height: "250px" }}
+      >
         <Image
           alt="gallery"
           className="absolute inset-0 w-full h-full object-cover object-center"
           src={imageUrl}
-          fill
+          layout="fill"
+          objectFit="cover"
         />
-        
+
         <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
           <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
             {subtitle}
@@ -58,9 +59,18 @@ const GalleryImageHolder = ({
           <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
             {title}
           </h1>
-          <button onClick={() => handleDownload(imageUrl,title)} className="mr-2 bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">Download</button>
-          <button onClick={() => handleDelete(imageUrl)} className="absolute top-0 right-0 m-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-full">X</button>
-
+          <button
+            onClick={() => handleDownload(imageUrl, title)}
+            className="mr-2 bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Download
+          </button>
+          <button
+            onClick={() => handleDelete(imageUrl)}
+            className="absolute top-0 right-0 m-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-full"
+          >
+            X
+          </button>
         </div>
       </div>
     </div>
